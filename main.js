c0294a5b4ee32e26d9b1f01985b46ebcd314dcaf
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-09-26 12:01:56 -0300

    dsadsa

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function soma (non1, non2){
     return non1+non2;
 }
 function areaquadrado (lado1,lado2){
-    return lado1*lado2;
+    return quanto**2;
 }
 console.log(soma(1,3));
 console.log(areaquadrado(30,30));
