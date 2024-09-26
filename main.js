92db5422ce3a2cc68a569c3e45d95a7006fe3536
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-09-26 12:26:38 -0300

    dsadsadsa

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let media = function(quanto1, quanto2, quanto3){
     return quanto1+quanto2+quanto3/3;
 
 }
-document(quanto1, quanto2, quanto3);
+
 console.log(soma(1,3));
 console.log(areaQ (quanto1));
 console.log(media(quanto1, quanto2, quanto3));
