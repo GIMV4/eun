baa460ac1eaa15ebbffe51e3e0676bd4b6dd275f
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-09-26 12:21:28 -0300

    dsadsadsa

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 alert('algo');
-let quanto = parseInt(prompt('insira um números'));
-document.write (quanto);
+let quanto1 = parseInt(prompt('insira um números'));
+document.write (quanto1);
 
+let quanto2 = parseInt(prompt('insira um números'));
+document.write (quanto2);
+
+let quanto3 = parseInt(prompt('insira um números'));
+document.write (quanto3);
 function soma (non1, non2){
     return non1+non2;
 }
-function areaQ (quanto){
-    return quanto**2;
+function areaQ (quanto1){
+    return quanto1**2;
+}
+let media = function(quanto1, quanto2, quanto3){
+    return quanto1+quanto2+quanto3/3;
 }
 
 console.log(soma(1,3));
-console.log(areaQ (quanto));
+console.log(areaQ (quanto1));
+console.log(media(quanto1, quanto2, quanto3));
