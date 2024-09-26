e9b335431e24e01ea50dc1fedf73dde7825478bc
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-09-26 12:06:51 -0300

    xz\xz\

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 alert('algo');
 let quanto = parseInt(prompt('insira um números'));
 document.write (quanto);
-let r = function (quanto){
-    return quanto**2;
-}
 function soma (non1, non2){
     return non1+non2;
 }
