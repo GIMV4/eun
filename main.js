39d9d137cd04029b33f712c6daee7de2cc54821b
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-09-26 12:13:46 -0300

    DSADS

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 alert('algo');
 let quanto = parseInt(prompt('insira um números'));
+document.write (quanto);
 
 function soma (non1, non2){
     return non1+non2;
@@ -7,6 +8,6 @@ function soma (non1, non2){
 function areaQ (quanto){
     return quanto**2;
 }
-document.write (quanto);
+
 console.log(soma(1,3));
 console.log(areaQ (quanto));
